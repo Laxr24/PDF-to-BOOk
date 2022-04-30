c3dc068b911743681ada068f3794debf3bd249ee
Laxr24 <73450829+Laxr24@users.noreply.github.com>
2022-04-30 20:42:21 +0600

    __

--- src/main.js
+++ src/main.js
@@ -77,7 +77,13 @@ function renderPage(currentPage) {
 
 }
 
-
+function numberCheck(num) {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 
@@ -101,69 +107,92 @@ function renderPage(currentPage) {
 
 // Printable book calculation asynchronous function 
 function bookLet(src, segment = 1) {
+
     var loadingTask = pdfjsLib.getDocument(src);
+
+    var output =[]
+
     loadingTask.promise.then(function (pdf) {
         let totalPage = pdf._pdfInfo.numPages;
         // let totalPage = 97;
         let sheet = Math.ceil(totalPage / 2);
         let segments = segment
 
+
+
+
         let pageOrder = []
         //[1,2,3,4,5,6,7,8,9,10,11,12]
         let reOrder = []
+        let even = []
+        let odd = []
+
+
         //[12,11,10,9,8,7,6,5,4,3,2,1]
         // Split the array in two half
         // Sequence join 
-        // [12,10,8] + [1,3,5]
+        // [12,10,8,2,4,6] + [1,3,5,11,9,7]
         // Flip log edge
         // [2,4,6]+[11,9,7]
         // [ (12,1), (10,3) , (8, 5) -- flip operation -- (2,11) , (4,9) , (6,7)  ]
 
-        for (let i = 1; i <= totalPage+1; i++) {
+        for (let i = 1; i <= totalPage + 1; i++) {
             pageOrder.push(i)
         }
 
         pageOrder.reverse()
 
-        pageOrder.forEach(i=>{
-            if(numberCheck(i)){
-                reOrder.push(i)
+        pageOrder.forEach(i => {
+            if (numberCheck(i)) {
+                even.push(i)
+            } else {
+                odd.push(i)
             }
         })
 
-        console.log(reOrder);
+        // REversing the odd array to match
+        odd.reverse()
 
-        let pair = [Math.floor(pageOrder.length/2)-1, Math.floor(pageOrder.length/2) ]
-        console.log(`Pair indexes are : (${pair})`)
-
-        console.log(`Pair value from arrayare : (${pageOrder[pair[0]]}, ${pageOrder[pair[1]]})`);
-
-
-        console.log(pageOrder);
-
-
-        if (segment > 1) {
-            console.log("The book will be printed in multiple segments: " + segments);
-            console.log("Total page: " + totalPage);
-            console.log("Printing the segment needs: " + Math.ceil(sheet/segments) + " Sheets");
-        } else {
-            console.log("Total page: " + totalPage);
-            console.log("Printing the book needs: " + sheet/2 + " Sheets");
+        for (let i = 0; i <= even.length - 1; i++) {
+            reOrder.push([
+                even[i],
+                odd[i]
+            ])
         }
 
+        // console.log(reOrder);
 
+        let final = []
+        let seg1 = []
+        let seg2 = []
+
+            for (let i = 0; i < reOrder.length; i++) {
+                if (i < reOrder.length / 2) {
+                    seg1.push(reOrder[i])
+                } else {
+                    seg2.push(reOrder[i])
+                }
+            }
+
+            seg1.forEach(element => {
+                if (element) {
+                    final.push(element)
+                }
+            });
+
+
+            final.push([0, 0])
+            seg2.reverse()
+
+            for (let i = seg2.length - 1; i >= 0; i--) {
+                final.push(seg2[i])
+            }
+
+            console.log(final)
+           
+
+        
     })
 }
 
 bookLet("test.pdf")
-
-
-
-function numberCheck(num){
-    if(num % 2 == 0){
-        return true; 
-    }
-    else{
-        return false; 
-    }
-}
